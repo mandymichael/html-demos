5f074a05e19d56a0f3f3d8e9cb1fa289c324f8f6
Mandy Michael <mandy.michael@wanews.com.au>
2018-07-17 16:29:27 +0800

    move date and time

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,61 +2,65 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const SecondPage = () => (
-  <div>
-    <header>
-        <h1>Dogs: They are good</h1>
-        <time datetime="2018-08-03">August 3rd, 2018</time>
-    </header>
+    <div>
 
-    <nav role="navigation">
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/good-dogs">Good Dogs</a></li>
-            <li><a href="/">Sleeping Dogs</a></li>
-            <li><a href="/">Fluffy Dogs</a></li>
-        </ul>
-    </nav>
+	<nav role="navigation">
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/good-dogs">Good Dogs</a></li>
+			<li><a href="/">Sleeping Dogs</a></li>
+			<li><a href="/">Fluffy Dogs</a></li>
+		</ul>
+	</nav>
 
-<main id="main" role="main" tabindex="0">
-        <article>
-            <h2>Why are dogs good?</h2>
-            <p>Doggo ipsum the neighborhood pupper, shoob you are doin me a concern. Noodle horse smol borking</p>
-            <figure>
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/Screen%20Shot%202018-04-02%20at%209.15.48%20PM.png" alt="A fluffy golden retriever" width="300" />
-                <figcaption>Michaelangelo AKA "Jello"</figcaption>
-            </figure>
-            <hr />
+	<main id="main" role="main" tabindex="0">
+			<h1>Dogs: They are good</h1>
+            <div><p>Corgo you are a long doggo, many pats.</p></div>
+		    <div><time datetime="2018-08-03">August 3rd, 2018</time></div>
 
-            <h3>Big ol pupper</h3>
+			<h2>Why are dogs good?</h2>
+			<p>Doggo ipsum the neighborhood pupper, shoob you are doin me a concern. Noodle horse smol borking</p>
+			<figure>
+				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/Screen%20Shot%202018-04-02%20at%209.15.48%20PM.png" alt="A fluffy golden retriever" width="300" />
+				<figcaption>Michaelangelo AKA "Jello"</figcaption>
+			</figure>
+			<hr />
 
-            <ul>
-                <li>Corgo you are a long doggo, many pats.</li>
-                <li>Blop adorable doggo what a nice floof</li>
-                <li>biscit smol borking doggo with a long snoot for pats</li>
-            </ul>
+			<h3>Big ol pupper</h3>
 
-            <h2>He made many woofs</h2>
-            <p>Shoober very good boys and girls smol aqua doggo</p>
-            <blockquote>
-                <p>Long doggo pupperino, many pats extremely cuuuuuute.</p>
-                <cite>– floofy pupper, neighborhood</cite>
-            </blockquote>
-            <p>Some extra content</p>
-            <p>Doggo ipsum aqua doggo bork such treat stop it fren bork doge, sub woofer shoober heckin angery woofer woofer. dat tungg tho wow very biscit floofs.  Floofs doing me a frighten woofer tungg aqua doggo, ur givin me a spook vvv long bois. Puggo aqua doggo shooberino borkf, lotsa pats.  Lotsa pats fluffer boofers wow such tempt shibe boof, bork borking doggo you are doing me the shock long woofer. Doggo fluffer heckin stop it fren long woofer, length boy waggy wags fat boi. Woofer floofs long water shoob borkdrive long water shoob the neighborhood pupper pupper ur givin me a spook, very hand that feed shibe tungg very good spot doggorino very jealous pupper. Ur givin me a spook big ol long doggo, he made many woofs. Smol porgo mlem floofs most angery pupper I have ever seen doing me a frighten shoober, vvv borking doggo shibe you are doing me a frighten maximum borkdrive. Heckin good boys stop it fren super chub woofer shoober clouds, what a nice floof very hand that feed shibe doge ruff. Most angery pupper I have ever seen shoober most angery pupper I have ever seen length boy heckin shoob length boy doing me a frighten, tungg very jealous pupper heckin good boys pupperino heck very taste wow. Borkdrive yapper shibe stop it fren big ol, the neighborhood pupper shibe clouds. Noodle horse lotsa pats puggo, bork. Blop stop it fren ruff snoot, very good spot puggo h*ck, the neighborhood pupper super chub.  Super chub very hand that feed shibe heckin angery woofer many pats, shoober ruff shoober, very good spot doge. Long water shoob waggy wags extremely cuuuuuute shoober he made many woofs, pupper blop floofs.  Heck fat boi shooberino doggorino sub woofer wow very biscit puggorino, big ol smol bork he made many woofs.</p>
-	    </article>
-    </main>
+			<ul>
+				<li>Corgo you are a long doggo, many pats.</li>
+				<li>Blop adorable doggo what a nice floof</li>
+				<li>biscit smol borking doggo with a long snoot for pats</li>
+			</ul>
 
-    <footer>
-        <section role="navigation">
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/good-dogs">Good Dogs</a></li>
-                <li><a href="/">Sleeping Dogs</a></li>
-                <li><a href="/">Fluffy Dogs</a></li>
-            </ul>
-        </section>
-    </footer>
-  </div>
+			<h2>He made many woofs</h2>
+			<p>Shoober very good boys and girls smol aqua doggo</p>
+			<blockquote>
+				<p>Long doggo pupperino, many pats extremely cuuuuuute.</p>
+				<cite>– floofy pupper, neighborhood</cite>
+			</blockquote>
+			<p>Some extra content</p>
+			<p>Doggo ipsum aqua doggo bork such treat stop it fren bork doge, sub woofer shoober heckin angery woofer woofer. dat tungg tho wow very biscit floofs. Floofs doing me a frighten woofer tungg aqua doggo, ur givin me a spook vvv long bois. Puggo aqua doggo
+				shooberino borkf, lotsa pats. Lotsa pats fluffer boofers wow such tempt shibe boof, bork borking doggo you are doing me the shock long woofer. Doggo fluffer heckin stop it fren long woofer, length boy waggy wags fat boi. Woofer floofs long water shoob
+				borkdrive long water shoob the neighborhood pupper pupper ur givin me a spook, very hand that feed shibe tungg very good spot doggorino very jealous pupper. Ur givin me a spook big ol long doggo, he made many woofs. Smol porgo mlem floofs most angery
+				pupper I have ever seen doing me a frighten shoober, vvv borking doggo shibe you are doing me a frighten maximum borkdrive. Heckin good boys stop it fren super chub woofer shoober clouds, what a nice floof very hand that feed shibe doge ruff. Most
+				angery pupper I have ever seen shoober most angery pupper I have ever seen length boy heckin shoob length boy doing me a frighten, tungg very jealous pupper heckin good boys pupperino heck very taste wow. Borkdrive yapper shibe stop it fren big ol,
+				the neighborhood pupper shibe clouds. Noodle horse lotsa pats puggo, bork. Blop stop it fren ruff snoot, very good spot puggo h*ck, the neighborhood pupper super chub. Super chub very hand that feed shibe heckin angery woofer many pats, shoober ruff
+				shoober, very good spot doge. Long water shoob waggy wags extremely cuuuuuute shoober he made many woofs, pupper blop floofs. Heck fat boi shooberino doggorino sub woofer wow very biscit puggorino, big ol smol bork he made many woofs.</p>
+	</main>
+
+	<footer>
+		<section role="navigation">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/good-dogs">Good Dogs</a></li>
+				<li><a href="/">Sleeping Dogs</a></li>
+				<li><a href="/">Fluffy Dogs</a></li>
+			</ul>
+		</section>
+	</footer>
+</div>
 )
 
 export default SecondPage
