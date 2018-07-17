ea13ebc30dd63aa72c17a8b0ca098a8c2d144469
Mandy Michael <mandy.michael@wanews.com.au>
2018-07-17 20:04:21 +0800

    test reader mode text length

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -26,10 +26,9 @@ const SecondPage = () => (
 			</figure>
 			<hr />
 
-			<h3>Big ol pupper</h3>
-
+			<h3>Doggo ipsum the neighborhood pupper, shoob you are doin me a concern. Noodle horse smol borking</h3>
 			<ul>
-				<li>Corgo you are a long doggo, many pats.</li>
+				<li>Doggo ipsum the neighborhood pupper, shoob you are doin me a concern. Noodle horse smol borking</li>
 				<li>Blop adorable doggo what a nice floof</li>
 				<li>biscit smol borking doggo with a long snoot for pats</li>
 			</ul>
@@ -40,14 +39,7 @@ const SecondPage = () => (
 				<p>Long doggo pupperino, many pats extremely cuuuuuute.</p>
 				<cite>– floofy pupper, neighborhood</cite>
 			</blockquote>
-			<p>Some extra content</p>
-			<p>Doggo ipsum aqua doggo bork such treat stop it fren bork doge, sub woofer shoober heckin angery woofer woofer. dat tungg tho wow very biscit floofs. Floofs doing me a frighten woofer tungg aqua doggo, ur givin me a spook vvv long bois. Puggo aqua doggo
-				shooberino borkf, lotsa pats. Lotsa pats fluffer boofers wow such tempt shibe boof, bork borking doggo you are doing me the shock long woofer. Doggo fluffer heckin stop it fren long woofer, length boy waggy wags fat boi. Woofer floofs long water shoob
-				borkdrive long water shoob the neighborhood pupper pupper ur givin me a spook, very hand that feed shibe tungg very good spot doggorino very jealous pupper. Ur givin me a spook big ol long doggo, he made many woofs. Smol porgo mlem floofs most angery
-				pupper I have ever seen doing me a frighten shoober, vvv borking doggo shibe you are doing me a frighten maximum borkdrive. Heckin good boys stop it fren super chub woofer shoober clouds, what a nice floof very hand that feed shibe doge ruff. Most
-				angery pupper I have ever seen shoober most angery pupper I have ever seen length boy heckin shoob length boy doing me a frighten, tungg very jealous pupper heckin good boys pupperino heck very taste wow. Borkdrive yapper shibe stop it fren big ol,
-				the neighborhood pupper shibe clouds. Noodle horse lotsa pats puggo, bork. Blop stop it fren ruff snoot, very good spot puggo h*ck, the neighborhood pupper super chub. Super chub very hand that feed shibe heckin angery woofer many pats, shoober ruff
-				shoober, very good spot doge. Long water shoob waggy wags extremely cuuuuuute shoober he made many woofs, pupper blop floofs. Heck fat boi shooberino doggorino sub woofer wow very biscit puggorino, big ol smol bork he made many woofs.</p>
+			<p>Doggo ipsum aqua doggo bork such treat stop it fren bork doge, sub woofer shoober heckin angery woofer woofer. dat tungg tho wow very biscit floofs. Floofs doing me a frighten woofer tungg aqua doggo, ur givin me a spook vvv long bois.</p>
 	</main>
 
 	<footer>
