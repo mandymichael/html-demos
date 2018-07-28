97cf6f67eb6032000a9494bbc8cd665e588b9ca7
Mandy Michael <mandy.michael@wanews.com.au>
2018-07-29 00:00:55 +0800

    Create good semantic page without roles and metadata

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -2,60 +2,66 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const SecondPage = () => (
-  <div>
-    <header>
-        <h1>Dogs: They are good</h1>
-        <time datetime="2018-08-03">August 3rd, 2018</time>
-    </header>
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/good-dogs">Good Dogs</a></li>
+                    <li><a href="/bad-dog-myths">Bad Dog Myths</a></li>
+                </ul>
+            </nav>
+        </header>
 
-    <nav role="navigation">
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/good-dogs">Good Dogs</a></li>
-            <li><a href="/">Sleeping Dogs</a></li>
-            <li><a href="/">Fluffy Dogs</a></li>
-        </ul>
-    </nav>
+        <main>
+            <h1>Dogs: They are good</h1>
+            <p>We are dedicated to educating the world on why dogs are good, and how they can make your life good.</p>
+            <div>
+                <span>Mandy Michael</span>,&nbsp;
+                <time datetime="2018-08-03">August 3rd, 2018</time>
+            </div>
 
-<main id="main" role="main" tabindex="0">
-        <section>
-            <h2>Why are dogs good?</h2>
-            <p>Doggo ipsum the neighborhood pupper, shoob you are doin me a concern. Noodle horse smol borking</p>
-            <figure>
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/Screen%20Shot%202018-04-02%20at%209.15.48%20PM.png" alt="A fluffy golden retriever" width="300" />
-                <figcaption>Michaelangelo AKA "Jello"</figcaption>
-            </figure>
-            <hr />
+            <article>
+                <h2>Why are dogs good?</h2>
+                <p>Dogs are loyal, intelligent, devoted and affectionate, they are known to improve our physical and mental health.</p>
 
-            <h3>Big ol pupper</h3>
+                <figure>
+                    <figcaption>A dog named Michaelangelo <abbr title="Also Known As">AKA</abbr> Jello</figcaption>
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/Screen%20Shot%202018-04-02%20at%209.15.48%20PM.png" alt="A fluffy golden retriever" width="300" />
+                </figure>
 
-            <ul>
-                <li>Corgo you are a long doggo, many pats.</li>
-                <li>Blop adorable doggo what a nice floof</li>
-                <li>biscit smol borking doggo with a long snoot for pats</li>
-            </ul>
+                <p>The <dfn><abbr title="International Good Dog Association">IGDA</abbr></dfn>
+is an international, not-for-profit organization responsible for determining that all dogs are good.</p>
 
-            <h2>He made many woofs</h2>
-            <p>Shoober very good boys and girls smol aqua doggo</p>
-            <blockquote>
-                <p>Long doggo pupperino, many pats extremely cuuuuuute.</p>
-                <cite>– floofy pupper, neighborhood</cite>
-            </blockquote>
-            <p>Doggo ipsum aqua doggo bork such treat stop it fren bork doge, sub woofer shoober heckin angery woofer woofer. dat tungg tho wow very biscit floofs.  Floofs doing me a frighten woofer tungg aqua doggo, ur givin me a spook vvv long bois. Puggo aqua doggo shooberino borkf, lotsa pats.  Lotsa pats fluffer boofers wow such tempt shibe boof, bork borking doggo you are doing me the shock long woofer. Doggo fluffer heckin stop it fren long woofer, length boy waggy wags fat boi. Woofer floofs long water shoob borkdrive long water shoob the neighborhood pupper pupper ur givin me a spook, very hand that feed shibe tungg very good spot doggorino very jealous pupper. Ur givin me a spook big ol long doggo, he made many woofs. Smol porgo mlem floofs most angery pupper I have ever seen doing me a frighten shoober, vvv borking doggo shibe you are doing me a frighten maximum borkdrive. Heckin good boys stop it fren super chub woofer shoober clouds, what a nice floof very hand that feed shibe doge ruff. Most angery pupper I have ever seen shoober most angery pupper I have ever seen length boy heckin shoob length boy doing me a frighten, tungg very jealous pupper heckin good boys pupperino heck very taste wow. Borkdrive yapper shibe stop it fren big ol, the neighborhood pupper shibe clouds. Noodle horse lotsa pats puggo, bork. Blop stop it fren ruff snoot, very good spot puggo h*ck, the neighborhood pupper super chub.  Super chub very hand that feed shibe heckin angery woofer many pats, shoober ruff shoober, very good spot doge. Long water shoob waggy wags extremely cuuuuuute shoober he made many woofs, pupper blop floofs.  Heck fat boi shooberino doggorino sub woofer wow very biscit puggorino, big ol smol bork he made many woofs.</p>
-	    </section>
-    </main>
+                <blockquote>
+                    <p>Dog owners are 34% more likely to get enough exercise than non-pet owners are.</p>
+                    <cite>– A smart person</cite>
+                </blockquote>
+                <hr />
 
-    <footer>
-        <section role="navigation">
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/good-dogs">Good Dogs</a></li>
-                <li><a href="/">Sleeping Dogs</a></li>
-                <li><a href="/">Fluffy Dogs</a></li>
-            </ul>
-        </section>
-    </footer>
-  </div>
+                <aside>
+                    <h2>Related Links</h2>
+                    <a href="/">IGDA</a>
+                </aside>
+
+                <h2>The benefits of owning a dog</h2>
+                <ul>
+                    <li>Studies have found dog owners have lower blood pressure than non-dog owners.</li>
+                    <li>Regular walks with your dog will result in better fitness.</li>
+                    <li>Dogs provide us with a sense of emotional wellbeing thanks to the unconditional love they give.</li>
+                    <li>When dogs socialise with other dogs, their owners make new friends and socialise with other people as well.</li>
+                </ul>
+
+                <h3>Did you know?</h3>
+                <p>There is evidence to suggest that we didn't domesticate wolves – they came to us first. The friendliest ones hung around our ancestors' campsites and so began the evolution of the modern dog.</p>
+
+            </article>
+        </main>
+
+        <footer>
+            <p>Made with love by Mandy Michael</p>
+        </footer>
+    </div>
 )
 
 export default SecondPage
